74f103ff51f1225c1cc400efbdca7a4106d39829
Suryansh2160 <91190803+Suryansh2160@users.noreply.github.com>
2026-08-21 13:48:32 +0530

    added to git

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,8 +12,8 @@ export default function App() {
   return (
     <AuthProvider>
       <Routes>
+        
         <Route path="/login" element={<Login />} />
-
         <Route
           element={
             <ProtectedRoute>
